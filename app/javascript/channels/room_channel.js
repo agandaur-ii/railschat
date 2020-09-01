dace6acb972804d03c53a399ec60d6c8d10d0d3c
Agandaur <cjwagstaffe@gmail.com>
2020-09-01 01:06:49 -0700

    chats now appear in the console

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -12,5 +12,7 @@ consumer.subscriptions.create("RoomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
+    console.log("Recieving:")
+    console.log(data.content)
   }
 });
